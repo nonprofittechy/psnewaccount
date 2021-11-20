16b444975029e201e334ca53c5b3b15a064d630b
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-11-20 10:49:40 -0500

    Update to new Legal Server API
    Removes the IE Com object automation and replaces with the much more robust Legal Server API

--- provisioning/new-legalserveraccount.ps1
+++ provisioning/new-legalserveraccount.ps1
@@ -8,7 +8,7 @@ This should be used carefully as users cannot be deleted from Legal Server. The 
 .PARAMETER newUserURL
   The URL for the first screen of the new user process
 .PARAMETER adminUsername
-  Login username for Legal Server with "add/edit users" permission
+  Login username for Legal Server with "both internal and external API add/edit users" permission
 .PARAMETER adminPassword
   Password matching username
 .PARAMETER username
@@ -35,12 +35,12 @@ This should be used carefully as users cannot be deleted from Legal Server. The 
   Internal database value for Legal Server user "Office"
 .EXAMPLE
 Example with list of user fields in a .CSV file for piped input:
-import-csv .\users.csv | new-legalserveraccount -username [USERNAME] -password [PASSWORD] -loginURL "https://contoso.legalserver.org" -newUserURL "https://contoso.legalserver.org/user/process/dynamic_XX"
+import-csv .\users.csv | new-legalserveraccount -username [USERNAME] -password [PASSWORD] -loginURL "https://contoso.legalserver.org"
 .EXAMPLE
-new-legalserveraccount -adminUsername [USERNAME] -adminPassword [PASSWORD] -loginURL "https://contoso.legalserver.org" -newUserURL "https://contoso.legalserver.org/user/process/dynamic_XX"
+new-legalserveraccount -adminUsername [USERNAME] -adminPassword [PASSWORD] -loginURL "https://contoso.legalserver.org" 
 .NOTES
 	Author: Quinten Steenhuis, 12/22/2017
-  Reference: https://cmdrkeene.com/automating-internet-explorer-with-powershell
+  Migrated to new Legal Server API on 11/20/2021
   see also: https://www.sepago.com/blog/2016/05/03/powershell-exception-0x800a01b6-while-using-getelementsbytagname-getelementsbyname
   https://msdn.microsoft.com/en-us/library/aa752084(v=vs.85).aspx
   
@@ -50,12 +50,12 @@ https://github.com/nonprofittechy/psnewaccount
 #>
 [CmdletBinding()]
 Param(
-  [Parameter(Mandatory = $True,HelpMessage="HomePage URL")][string]$loginURL,
-  [Parameter(Mandatory = $True,HelpMessage="New User Process URL")][string]$newUserURL,
+  [Parameter(Mandatory = $True,HelpMessage="Legal Server base URL, like: https://myorg-demo.legalserver.org")][string]$loginURL,
+  [Parameter][string]$newUserURL, # Not used, left in for possible backwards compatibility reasons
   [Parameter(Mandatory = $True)][string]$adminUsername,
   [Parameter(Mandatory = $True)][string]$adminPassword,
   [Parameter(Mandatory = $True,ValueFromPipelinebyPropertyName=$True)][string]$username,
-  [Parameter(Mandatory = $True,ValueFromPipelinebyPropertyName=$True)][string]$password,
+  [Parameter(ValueFromPipelinebyPropertyName=$True)][string]$password, # Not used
   [Parameter(Mandatory = $True,ValueFromPipelinebyPropertyName=$True)][string]$LSProgramID,
   [Parameter(Mandatory = $True,ValueFromPipelinebyPropertyName=$True)][string]$LSTypeID,
   [Parameter(Mandatory = $True,ValueFromPipelinebyPropertyName=$True)][string]$LSRoleID,
@@ -69,148 +69,26 @@ Param(
   
 )
 Begin {
-  
-  # Legal Server displays a list with the class name "form_errors" if anything goes wrong
-  function get-legalservererrors ($ie) {
-    return ($ie.document.IHTMLDocument3_getElementsByTagName("ul")) | %{if ($_.className -eq "form_errors") {$_.innerHtml} }
-  }
-  
-  function test-legalservererrors ($ie) {
-    $e = get-legalservererrors $ie
-    if ( $e ) {
-      throw ($e -replace '<[^>]+>|(\&nbsp\;)','') + " : Skipping Legal Server account for username " +  $username + ", Name: " + $givenname + " " + $surname # clean the HTML tags from the error in Legal Server
-    }
-  }
-  
-  ########################################################
-  # Customize the variable below if needed for your Legal Server site. Many of the field 
-  # names appear to be standardized and may not need to be altered.
-  # double-check the "Do Not Set Organization" button ID for your site.
-  $fields = 
-  @{
-    "login_form" = "login_form";
-    "login_username" = "____login_login";
-    "login_password" = "____login_password";
-    "login_submit" = "____login_submit";
-    "user_login" = "user:login";
-    "user_type" = "user:types";
-    "user_role" = "user:role";
-    "user_office" = "user:office_id";
-    "user_program" = "user:program_id";
-    "user_expiration_date" = "user:date_end";
-    "user_first_name" = "contact:first";
-    "user_middle_name" = "contact:middle";
-    "user_last_name" = "contact:last";
-    "user_email" = "user:email";
-    "user_phone" = "user:phone_business";
-    "organization_donotsetbutton" = "form_element_toggler_5_input";
-    "user_password" = "____password";
-    "user_confirm_password" = "____confirm_password";
-  }
 
-  # Create new IE COM object and login to Legal Server
-  $ie = New-Object -com internetexplorer.application;
-  if ($PSBoundParameters.debug) {
-    $ie.visible = $true;
-  }
-  $ie.navigate($loginUrl);
-  while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }
-  
-  # check for the login form - if it's not present, check to see if there is an "admin" link. If not--something went wrong.
-  if ( ($ie.Document.IHTMLDocument3_getElementsByName($fields['login_form']) | select -first 1) ) {
-    ($ie.document.IHTMLDocument3_getElementsByName($fields['login_username']) |select -first 1).value = $adminUsername
-    ($ie.document.IHTMLDocument3_getElementsByName($fields['login_password']) |select -first 1).value = $adminPassword
-    ($ie.document.IHTMLDocument3_getElementsByName($fields['login_submit']) |select -first 1).click()
-  } elseif ( $ie.Document.IHTMLDocument3_getElementsByTagName("a") | where {$_.innerText -eq "Admin"} | select -first 1) {
-    # we are already logged in
-  } else {
-    throw "Something went wrong trying to access the main login page."
-  }
-  
-  while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }
-  #Start-sleep -seconds 10
 }
 
 Process {
   try {
 
-  $ie.navigate($newUserURL);
-  while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }
-  
-  # Page 1 - System Information
-  ($ie.document.IHTMLDocument3_getElementsByName($fields['user_login']) |select -first 1).value = ($username.tolower())
-  if ($accountExpirationDate) {
-    $formattedDate = get-date $accountExpirationDate -format "MM/dd/yyyy"
-    ($ie.document.IHTMLDocument3_getElementsByName($fields['user_expiration_date']) |select -first 1).value = $formattedDate
-  }
-  ($ie.document.IHTMLDocument3_getElementsByName($fields['user_type']) |select -first 1).value = $LSTypeID
-  ($ie.document.IHTMLDocument3_getElementsByName($fields['user_role']) |select -first 1).value = $LSRoleID
-  ($ie.document.IHTMLDocument3_getElementsByName($fields['user_office']) |select -first 1).value = $LSOfficeID
-  ($ie.document.IHTMLDocument3_getElementsByName($fields['user_program']) |select -first 1).value = $LSProgramID
-  
-  write-debug "Submitting Page 1"
-  
-  ($ie.document.IHTMLDocument3_getElementsByName("submit_button") |select -first 1).click()
-  while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }
-  
-  test-legalservererrors $ie
-   
-  #Page 2 - Contact Information
-  # Check to see if we have a first name field
-  if ( ($ie.document.IHTMLDocument3_getElementsByName($fields['user_first_name']) |select -first 1) ) {
-    ($ie.document.IHTMLDocument3_getElementsByName($fields['user_first_name']) | select -first 1).value = $givenName
-    ($ie.document.IHTMLDocument3_getElementsByName($fields['user_last_name']) | select -first 1).value = $surname
-    ($ie.document.IHTMLDocument3_getElementsByName($fields['user_middle_name']) | select -first 1).value = $middleName
-    ($ie.document.IHTMLDocument3_getElementsByName($fields['user_email']) | select -first 1).value = $emailAddress
-    ($ie.document.IHTMLDocument3_getElementsByName($fields['user_phone']) | select -first 1).value = $phone
-    
-    write-debug "Submitting Page 2"  
-    ($ie.document.IHTMLDocument3_getElementsByName("submit_button") |select -first 1).click()
-    while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }
-    
-    test-legalservererrors $ie
-  
-    
-    
-  } else {
-    write-error "Unexpected page - expected to see a field named 'contact:first'."
-    break;
-  }
-  
-  #Page 3 - Organization Affiliation
-  if (($ie.document.IHTMLDocument3_getElementById($fields["organization_donotsetbutton"])) ) {
-    # click the "Do not set Organization" button
-    ($ie.document.IHTMLDocument3_getElementById($fields["organization_donotsetbutton"])).click()
-   
-    write-debug "Submitting Page 3"
-    ($ie.document.IHTMLDocument3_getElementsByName("submit_button") |select -first 1).click()
-    while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }
-    
-    test-legalservererrors $ie
-   
-  } else {
-      write-error "Unexpected page - expected to be on the Organization Affiliation page"
-      break;
-  }
-  
-  #Page 4 - Password
-  if (($ie.document.IHTMLDocument3_getElementsByName($fields['user_password']))) {
-    # Set the "Needs to change password" radio to Yes
-    ($ie.document.IHTMLDocument3_getElementsByTagName("input") | where {$_.type -eq "radio" -and $_.value -like "t"} | select -first 1).checked = $true
-    ($ie.document.IHTMLDocument3_getElementsByName($fields['user_password']) | select -first 1).value = $password
-    ($ie.document.IHTMLDocument3_getElementsByName($fields['user_confirm_password']) | select -first 1).value = $password
-    
-    write-debug "Submitting Page 4"
-    ($ie.document.IHTMLDocument3_getElementsByName("submit_button") |select -first 1).click()
-    
-    while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }
-    
-    test-legalservererrors $ie
+  $user_params = @{ 
+    "first"= $givenName
+    "middle"= $middleName
+    "last"= $surname
+    "login"= $username
+    "email"= $emailaddress
+    "role"= $LSRoleID
+    "types"= $LSTypeID
+    "office"= $LSOfficeID
+    "program"= $LSProgramID
+    "password_reset"= $true 
+  }    
 
-  } else {
-    write-error "Unexpected page - expected to be on the Password page."
-    break;
-  }
+  invoke-webrequest -Method POST -Uri ($loginURL + "/api/v1/users") -Headers @{ Authorization = "Basic "+ [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($adminUsername):$($adminPassword)")); "Content-Type"="application/json"} -Body ($user_params | convertto-json)
   
   } catch {
     write-error $_
@@ -219,6 +97,5 @@ Process {
 }
 
 End {
-  write-debug "Closing IE"
-  $ie.quit()
+  write-debug "All done"
 }
